0c70c08be540ed0b9438b5839f4c681e87851e07
Jack <jacksonmorgan@hey.com>
2021-02-12 10:17:06 -0500

    add languages and skill images to home compoenent

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import {v4 as uuidv4} from 'uuid'
 
 export default function Home() {
+  const languages = ['css3', 'html5', 'javascript', 'postgres', 'rails', 'ruby', 'react', 'redux']
   return(
     <div className='home'>
       <p className='site-welcome'>Welcome!</p>
-      <div className='self'>
-
+      <div className='self'></div>
+      <div className='skills'>
+        {languages.map((item) => {
+          return <div key={uuidv4()} id='language' className={`${item}`}/>
+        })}
       </div>
     </div>
   )
